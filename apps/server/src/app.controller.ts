422b135893ba1cda6cba0d45f6cb4506c1495993
yn1323 <yn1323@gmail.com>
2022-03-03 20:53:06 +0900

    infra

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,12 +1,36 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+const serviceAccount = require('../serviceAccountKey.json');
+
+import { initializeApp,  applicationDefault, cert } from 'firebase-admin/app';
+import admin from 'firebase-admin';
+import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
+
+// initializeApp({
+//   credential: cert(serviceAccount)
+// });
+initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+const db = getFirestore();
+
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    const docRef = db.collection('users').doc('alovelace');
+    await docRef.set({
+      first: 'Ada',
+      last: 'Lovelace',
+      born: 1815
+    });
+    return process?.env?.token_uri ?? 'no';
   }
 }
