402c3ca66ab7b93b814b0dc99bf1b44a46377f7b
yn1323 <yn1323@gmail.com>
2022-03-03 20:55:54 +0900

    infra

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,21 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-const serviceAccount = require('../serviceAccountKey.json');
 
-import { initializeApp,  applicationDefault, cert } from 'firebase-admin/app';
-import admin from 'firebase-admin';
-import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
+// import { initializeApp,  applicationDefault, cert } from 'firebase-admin/app';
+// import admin from 'firebase-admin';
+// import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
 
+// // initializeApp({
+// //   credential: cert(serviceAccount)
+// // });
 // initializeApp({
-//   credential: cert(serviceAccount)
+//   credential: admin.credential.cert(serviceAccount)
 // });
-initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
 
-const db = getFirestore();
+// const db = getFirestore();
 
 
 @Controller()
@@ -25,12 +24,12 @@ export class AppController {
 
   @Get()
   async getHello() {
-    const docRef = db.collection('users').doc('alovelace');
-    await docRef.set({
-      first: 'Ada',
-      last: 'Lovelace',
-      born: 1815
-    });
+    // const docRef = db.collection('users').doc('alovelace');
+    // await docRef.set({
+    //   first: 'Ada',
+    //   last: 'Lovelace',
+    //   born: 1815
+    // });
     return process?.env?.token_uri ?? 'no';
   }
 }
