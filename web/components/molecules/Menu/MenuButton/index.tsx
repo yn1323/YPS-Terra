eff7356b266734cf0840fa787d26c5955764a193
yn1323 <yn1323@gmail.com>
2022-03-12 10:34:10 +0900

    feat: menus

--- web/components/molecules/Menu/MenuButton/index.tsx
+++ web/components/molecules/Menu/MenuButton/index.tsx
@@ -1,14 +1,56 @@
 import { css } from '@emotion/react'
 import type { SerializedStyles } from '@emotion/react'
-import { FC } from 'react'
+import { KeyboardArrowDown } from '@mui/icons-material'
+import { Button } from '@mui/material'
+import { FC, useState, useRef } from 'react'
+import { ListMenu } from '@/atoms/List/ListMenu'
+import { Popper } from '@/atoms/Text/Popper'
+import { MenuItem } from '@/ui/layout/menu'
+import { themes } from '@/ui/theme'
 
 type PropTypes = {
   _css?: SerializedStyles | SerializedStyles[]
+  icon: JSX.Element
+  items?: MenuItem[]
+  delimeterPosition?: number[]
+  hasMenu: boolean
+  link: string
 }
 
-export const MenuButton: FC<PropTypes> = ({ _css }) => {
-  return <div css={[_css, styles.container]} data-testid="menubutton"></div>
+export const MenuButton: FC<PropTypes> = ({
+  _css,
+  icon,
+  items = [],
+  delimeterPosition,
+  hasMenu,
+  link,
+  children,
+}) => {
+  const [show, setShow] = useState(false)
+  const ref = useRef(null)
+  return (
+    <div css={_css}>
+      <Button
+        css={styles.button}
+        startIcon={icon}
+        endIcon={hasMenu ? <KeyboardArrowDown /> : undefined}
+        href={link ? link : undefined}
+        onClick={!link ? () => setShow(true) : undefined}
+        ref={ref}
+        data-testid="button"
+      >
+        {children}
+      </Button>
+      {!!items.length && (
+        <Popper show={show} setShow={setShow} anchorEl={ref.current}>
+          <ListMenu items={items} delimeterPosition={delimeterPosition} />
+        </Popper>
+      )}
+    </div>
+  )
 }
 const styles = {
-  container: css``,
+  button: css`
+    color: ${themes.palette.text.secondary};
+  `,
 }
