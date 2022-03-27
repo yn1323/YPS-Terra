4a88aa3382b26c3ed9092e69cc846944154ab323
yn1323 <yn1323@gmail.com>
2022-03-27 13:26:14 +0900

    actions

--- web/src/config/env.ts
+++ web/src/config/env.ts
@@ -1,10 +1,21 @@
 export const env = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  apiKey:
+    process.env.NEXT_PUBLIC_API_KEY ||
+    process.env.STORYBOOK_NEXT_PUBLIC_API_KEY,
+  authDomain:
+    process.env.NEXT_PUBLIC_AUTH_DOMAIN ||
+    process.env.STORYBOOK_NEXT_PUBLIC_API_KEY,
+  projectId:
+    process.env.NEXT_PUBLIC_PROJECT_ID ||
+    process.env.STORYBOOK_NEXT_PUBLIC_API_KEY,
+  storageBucket:
+    process.env.NEXT_PUBLIC_STORAGE_BUCKET ||
+    process.env.STORYBOOK_NEXT_PUBLIC_API_KEY,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID ||
+    process.env.STORYBOOK_NEXT_PUBLIC_API_KEY,
+  appId:
+    process.env.NEXT_PUBLIC_APP_I || process.env.STORYBOOK_NEXT_PUBLIC_API_KEYD,
 }
 
 export const isProduction = !env.projectId?.includes('development')
