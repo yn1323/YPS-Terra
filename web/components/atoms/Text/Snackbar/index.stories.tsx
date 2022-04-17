6751568455e7bf5da3e034041edfc2d898e826d2
yn1323 <yn1323@gmail.com>
2022-04-17 14:15:24 +0900

    feat: snackbar

---
+++ web/components/atoms/Text/Snackbar/index.stories.tsx
@@ -0,0 +1,16 @@
+import type { AlertColor } from '@mui/material'
+import { ComponentStoryObj, ComponentMeta } from '@storybook/react'
+import { Snackbar } from '.'
+
+type StoryObj = ComponentStoryObj<typeof Snackbar>
+export default {
+  title: 'atoms/Text/Snackbar',
+  component: Snackbar,
+} as ComponentMeta<typeof Snackbar>
+const args = {
+  children: 'Succeeded',
+  show: true,
+  severity: 'success' as AlertColor,
+}
+
+export const Basic: StoryObj = { args }
