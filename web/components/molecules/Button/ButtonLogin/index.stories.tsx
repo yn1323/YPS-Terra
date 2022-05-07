ab5b2582ba66c452a0dfe1d074664557e94a0a05
yn1323 <yn1323@gmail.com>
2022-05-07 11:28:15 +0900

    chore: storybook

--- web/components/molecules/Button/ButtonLogin/index.stories.tsx
+++ web/components/molecules/Button/ButtonLogin/index.stories.tsx
@@ -1,5 +1,4 @@
 import { ComponentStoryObj, ComponentMeta } from '@storybook/react'
-import { within, userEvent, screen } from '@storybook/testing-library'
 import { ButtonLogin } from '.'
 
 type StoryObj = ComponentStoryObj<typeof ButtonLogin>
@@ -9,19 +8,3 @@ export default {
 } as ComponentMeta<typeof ButtonLogin>
 
 export const Basic: StoryObj = {}
-
-export const Demo: StoryObj = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-
-    await userEvent.type(canvas.getByTestId('buttonlogin'), 'somevalue', {
-      delay: 300,
-    })
-    await userEvent.type(canvas.getByTestId('age'), '20', {
-      delay: 300,
-    })
-    await userEvent.selectOptions(canvas.getByTestId('sex'), '1')
-    await screen.findByText('登録')
-    await userEvent.click(canvas.getByRole('button'))
-  },
-}
