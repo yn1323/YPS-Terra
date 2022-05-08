535ab086eebe3068bbd164ec4d2dfd70f689ab78
yn1323 <yn1323@gmail.com>
2022-05-08 23:20:10 +0900

    feat: stepper

---
+++ web/components/templates/Step/index.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentStoryObj, ComponentMeta } from '@storybook/react'
+import { Step } from '.'
+
+type StoryObj = ComponentStoryObj<typeof Step>
+export default {
+  title: 'templates/Step',
+  component: Step,
+  decorators: [
+    Story => (
+      <div style={{ width: '100%', height: 'calc(100vh - 100px)' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Step>
+const args = {
+  labels: ['Step 1', 'Step 2', 'Step 3'],
+  children: [
+    <div key={0}>Step 1</div>,
+    <div key={1}>Step 2</div>,
+    <div key={2}>Step 3</div>,
+  ],
+  defaultStep: 0,
+}
+
+export const Basic: StoryObj = {
+  args,
+}
---
+++ web/components/templates/Step/index.tsx
@@ -0,0 +1,55 @@
+import { Box, Button, HStack, VStack } from '@chakra-ui/react'
+import { FC, useState } from 'react'
+import { StepButton } from '@/atoms/Step/StepButton'
+
+type PropTypes = {
+  labels: string[]
+  children: JSX.Element[]
+  defaultStep?: number
+}
+
+export const Step: FC<PropTypes> = ({ labels, children, defaultStep = 0 }) => {
+  if (labels.length !== children.length) {
+    console.error('Step: labels and children must be same length')
+  }
+  const [currentStep, setCurrentStep] = useState(defaultStep)
+
+  return (
+    <VStack
+      h={['100%', '100%', '500px']}
+      w="100%"
+      spacing={4}
+      justifyContent="space-between"
+    >
+      <VStack spacing={4} w="100%">
+        <StepButton
+          labels={labels}
+          currentStep={currentStep}
+          nextStep={setCurrentStep}
+        />
+        <Box>{children[currentStep]}</Box>
+      </VStack>
+      <HStack w="100%" justifyContent="space-between">
+        <Box>
+          {currentStep > 0 && (
+            <Button onClick={() => setCurrentStep(currentStep - 1)}>
+              前へ
+            </Button>
+          )}
+        </Box>
+
+        <Box>
+          {currentStep < children.length - 1 && (
+            <Button onClick={() => setCurrentStep(currentStep + 1)}>
+              次へ
+            </Button>
+          )}
+
+          {currentStep === children.length - 1 && (
+            <Button onClick={() => alert('登録')}>登録</Button>
+          )}
+        </Box>
+      </HStack>
+    </VStack>
+  )
+}
